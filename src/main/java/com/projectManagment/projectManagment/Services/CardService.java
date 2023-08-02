8549a64f4dbfcfe07a171d80f95b9866ca8d0e3d
Mohammed <moody.busaidi99@gmail.com>
2023-08-02 13:37:36 +0400

    created update card method

--- src/main/java/com/projectManagment/projectManagment/Services/CardService.java
+++ src/main/java/com/projectManagment/projectManagment/Services/CardService.java
@@ -23,4 +23,17 @@ public class CardService {
     public List<Card> getAllCards() {
         return cardRepository.findAll();
     }
+    public Card getCardById(Long cardId) {
+        return cardRepository.findById(cardId).get();
+    }
+
+    public Card updateCard(Long cardId, Card card) {
+        LocalDateTime now = LocalDateTime.now();
+        Card updateCard = cardRepository.findById(cardId).get();
+        updateCard.setTitle(card.getTitle());
+        updateCard.setSection(card.getSection());
+        updateCard.setDescription(card.getDescription());
+        updateCard.setBoard(card.getBoard());
+        return cardRepository.save(card);
+    }
 }
