b71fb1583d9fc0761c61e6ab85f162d93423cc0e
Mohammed <moody.busaidi99@gmail.com>
2023-08-02 12:00:35 +0400

    created get board request

--- src/main/java/com/projectManagment/projectManagment/Controller/BoardController.java
+++ src/main/java/com/projectManagment/projectManagment/Controller/BoardController.java
@@ -1,14 +1,29 @@
 package com.projectManagment.projectManagment.Controller;
 
+import com.projectManagment.projectManagment.Models.APICustomResponse;
+import com.projectManagment.projectManagment.Models.Board;
 import com.projectManagment.projectManagment.Services.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("api/boards")
-public class BoardController {
+public class BoardController extends GenericController {
     @Autowired
     BoardService boardService;
 
+    @GetMapping
+    public ResponseEntity<APICustomResponse> getAllBoards() {
+        List<Board> boards = boardService.getAllBoards();
+        return createResponse(
+                Map.of("boards", boards),
+                "List of all boards",
+                OK);
+    }
 }
