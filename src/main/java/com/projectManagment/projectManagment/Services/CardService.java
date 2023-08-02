750065e5322472a477f73f7f20c19a595c7c1837
Mohammed <moody.busaidi99@gmail.com>
2023-08-02 13:33:05 +0400

    created get all cards

--- src/main/java/com/projectManagment/projectManagment/Services/CardService.java
+++ src/main/java/com/projectManagment/projectManagment/Services/CardService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class CardService {
@@ -17,5 +18,9 @@ public class CardService {
         card.setActive(true);
         cardRepository.save(card);
         return card.getCardId();
+    }
+
+    public List<Card> getAllCards() {
+        return cardRepository.findAll();
     }
 }
